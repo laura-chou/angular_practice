be62dd87bf5dbf2a023effbbaca6c47767d0c8f2
Yu-Lun <chou522128@gmail.com>
2024-07-07 22:35:22 +0800

    creating an interface

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
-  templateUrl: './housing-location.component.html',
+  imports: [CommonModule],
+  template: `
+    <p>housing-location works!</p>
+  `,
   styleUrl: './housing-location.component.css'
 })
 export class HousingLocationComponent {
